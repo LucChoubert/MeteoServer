16f15519b1b5bdaa9bbc62275c0924464dc0231e
LucChoubert <luc.choubert@gmail.com>
2018-06-24 18:51:24 +0200

    Redis connection

--- MeteoServer.py
+++ MeteoServer.py
@@ -1,21 +1,58 @@
+import os
+
 from flask import Flask, request
 import MeteoFranceInterface.MeteoFranceInterface
 
+import redis
+
+def handleRedisCnx():
+    global redisCnxStatus
+    global conn
+    try:
+        if conn is None:
+            print(os.getenv('REDIS_HOST', 'localhost'))
+            conn = redis.StrictRedis(
+                host=os.getenv('REDIS_HOST', 'localhost'),
+                port=6379)
+        conn.ping()
+        redisCnxStatus = 'OK'
+    except Exception as exception:
+        redisCnxStatus = 'Error - ' + exception.__class__.__name__
+        conn = None
+
+
 app = Flask(__name__)
 
+redisCnxStatus = None
+conn = None
+
+handleRedisCnx()
+
+print('CECI EST UN TEST')
+
 @app.route("/")
-def hello():
-    var = "Server Up and Running\n" + request.user_agent.string + "\n"
+def root():
+    var = "OK\n"
+    return var
+
+@app.route("/status")
+def status():
+    handleRedisCnx()
+    var = "API Flask Server Up and Running\n"
+    var = var + "Connection from: " + request.user_agent.string + "\n"
+    var = var + "Redis Connection: " + redisCnxStatus + "\n"
     return var
 
 @app.route("/meteo/biot/web")
 def meteo1():
+    handleRedisCnx()
     aCityCode = MeteoFranceInterface.MeteoFranceInterface.getCityCodeFromName("biot")
     table = MeteoFranceInterface.MeteoFranceInterface.getDataFromMeteoFranceAPI( aCityCode )
     return table.get_html_string()
 
 @app.route("/meteo/biot/string")
 def meteo2():
+    handleRedisCnx()
     aCityCode = MeteoFranceInterface.MeteoFranceInterface.getCityCodeFromName("biot")
     table = MeteoFranceInterface.MeteoFranceInterface.getDataFromMeteoFranceAPI( aCityCode )
     table = table
