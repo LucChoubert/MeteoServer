3a93f3b25389ef148162997a33afcb44749c12f1
LucChoubert <luc.choubert@gmail.com>
2018-06-24 18:55:35 +0200

    docker inputs

---
+++ runInDocker.sh
@@ -0,0 +1,9 @@
+
+docker run --name api_server -d -p 5000:5000 meteoserver
+
+
+#docker pull redis
+docker run --name redis-server -d redis -p 6379:6379
+# docker exec -i -t 8dfef789c123 /bin/bash
+
+
