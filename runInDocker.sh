448c9632c75316f92c974c23820c00f5f8fef35f
LucChoubert <luc.choubert@gmail.com>
2018-06-30 21:00:09 +0200

    docker inputs

--- runInDocker.sh
+++ runInDocker.sh
@@ -1,9 +1,19 @@
+docker stop api_server
+docker rm api_server
 
-docker run --name api_server -d -p 5000:5000 meteoserver
+docker stop redis_server
+docker rm redis_server
 
+docker build -t meteoserver .
 
-#docker pull redis
-docker run --name redis-server -d redis -p 6379:6379
+docker network create --driver bridge application-net
+
+docker pull redis
+docker run --name redis_server --network application-net -d -p 6379:6379 redis
+
+docker run --name api_server --network application-net -d -p 5000:5000 meteoserver
+
+# connect for debugging
 # docker exec -i -t 8dfef789c123 /bin/bash
 
 
