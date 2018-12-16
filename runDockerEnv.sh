206ffd590ab43f253e750fd2b1464fd2ec8db0e1
Luc Choubert <luc.choubert@gmail.com>
2018-12-16 13:58:42 +0100

    add Portainer in the Env

--- runDockerEnv.sh
+++ runDockerEnv.sh
@@ -18,13 +18,25 @@ docker rm redis-commander
 docker network create --driver bridge application-net
 
 docker pull redis
+
+
+
+#RUN the various elements of the environment
+#Redis Server
 docker run --name redis-server --network application-net -d -p 6379:6379 redis
 
+#Meteo API Server
 docker run --name api-server --network application-net -d -p 5000:5000 meteoserver
 
+#Daemon getting the data from MeteoFrance and storing in Redis
 docker run --name retriever-daemon --network application-net -d meteodaemon
 
+#Web Interface for Redis database view
 docker run --name redis-commander --network application-net --env REDIS_HOSTS=PRD:redis-server:6379 -d -p 8081:8081 redis-commander:arm
+
+#Web Interface for Docker GUI administration
+docker run --name portainer --network application-net -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
+
 
 docker ps
 
